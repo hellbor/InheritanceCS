512e824d8f160d7244c29db0e3e51211a1f8a271
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-10-10 21:27:02 +0300

    Add project files.

---
+++ Acadamy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acadamy
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Human human = new Human("Montana", "Antonio", 25);
+			human.Print();
+            Console.WriteLine(human);
+
+            Student student = new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 96);
+			student.Print();
+            Console.WriteLine(student);
+
+            Teacher teacher = new Teacher("White", "Walter", 50, "Chemistry", 25);
+			teacher.Print();
+            Console.WriteLine(teacher);
+        }
+	}
+}
---
+++ Files/Program.cs
@@ -0,0 +1,22 @@
+﻿#define WRITE_TO_FILE
+#define READ_FROM_FILE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;	//Подключаем файловые потоки
+
+namespace Files
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+#if WRITE_TO_FILE
+
+#endif
+		}
+	}
+}
